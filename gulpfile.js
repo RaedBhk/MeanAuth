9cc7b595b064b3bfbf1a85ef102d2fafb1f087f9
wassim-saadallah <wassim.saadalla@gmail.com>
2017-03-02 00:35:42 +0100

    Added jshint as gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var server = require('gulp-express');
 var mocha = require('gulp-mocha');
 var util = require('gulp-util');
+var istanbul = require('gulp-istanbul');
+var jshint = require('gulp-jshint');
  
 gulp.task('server', function () {
     // Start the server at the beginning of the task 
@@ -14,4 +16,25 @@ gulp.task('test' , function () {
     return gulp.src(['test/**/*.js'], { read: false })
         .pipe(mocha({ reporter: 'spec' }))
         .on('error', util.log);
+});
+
+
+gulp.task('testing', function (cb) {
+  gulp.src(['src/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire()) 
+    .on('finish', function () {
+      gulp.src(['test/**/*.js'])
+        .pipe(mocha({ reporter: 'spec' }))
+	.pipe(istanbul.writeReports())
+	.pipe(istanbul.enforceThresholds({ thresholds: { global: 10 } }))
+	.on('end', cb);
+    });
+});
+
+
+gulp.task('lint', function() {
+  return gulp.src(['src/**/*.js', 'test/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
